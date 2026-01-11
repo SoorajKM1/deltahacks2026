c9c75480515f86b2d1fc8ea44e9a0274f0fa3a1c
Maanas Rao Siripuram <maanas710@gmail.com>
2026-01-10 22:19:26 -0500

    Integrated ElevenLabs

--- NeuroVault/patient-ui/models/Memory.ts
+++ NeuroVault/patient-ui/models/Memory.ts
@@ -3,7 +3,7 @@ import mongoose, { Schema } from 'mongoose';
 const MemorySchema = new Schema({
   text: { type: String, required: true },
   author: { type: String, default: "Caregiver" },
-  // We save the image as a huge text string (Base64) - easiest for Hackathons!
+  // We save the image as a huge text string (Base64) 
   image: { type: String }, 
   timestamp: { type: Date, default: Date.now },
 });
